a66c823cf6ccadb27f4a41508cf96730b4e81c84
Chad Arimura <carimura@gmail.com>
2019-06-18 17:47:06 -0700

    updating for py3.6

--- slack/post-to-slack/func.py
+++ slack/post-to-slack/func.py
@@ -1,59 +1,49 @@
-import fdk
+from fdk import response
 import json
-import slackclient
 import os
 import sys
 import ujson
 import time
+import io
+import asyncio
+import slack
 
 SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
 SLACK_CHANNEL = os.environ.get("SLACK_CHANNEL")
 
+sc = slack.WebClient(SLACK_TOKEN, run_async=True)
 
-def post_image_to_slack(msg, image_url):
-    sc = slackclient.SlackClient(SLACK_TOKEN)
-    return sc.api_call(
-        "chat.postMessage",
+async def post_image_to_slack(msg, image_url):
+    res = await sc.chat_postMessage(
         channel=SLACK_CHANNEL,
         text=msg,
         attachments=ujson.dumps([{"title": "", "image_url": image_url}]))
+    return res
 
-def post_msg_to_slack(msg):
-    sc = slackclient.SlackClient(SLACK_TOKEN)
-    return sc.api_call(
-        "chat.postMessage",
+async def post_msg_to_slack(msg):
+    res = await sc.chat_postMessage(
         channel=SLACK_CHANNEL,
         text=msg)
+    return res
 
-def handler(ctx, data=None, loop=None):
+async def handler(ctx, data: io.BytesIO=None, loop=None):
     if data is not None or len(data) != 0:
-        data = ujson.loads(data)
-        sys.stderr.write(
-            "payload to post-to-slack: {0}\n".format(ujson.dumps(data)))
+        data = ujson.loads(data.getvalue())
+        sys.stderr.write("payload to post-to-slack: {0}\n".format(ujson.dumps(data)))
         msg = data.get("msg")
 
     if not msg:
         msg = "Muy Guapo!"
 
     if not data.get("image_url"):
-        resp = post_msg_to_slack(msg)
+        res = await post_msg_to_slack(msg)
     else:
-        resp = post_image_to_slack(msg, data.get("image_url"))
-
-    # if "ok" in resp and resp["ok"]:
-    #     sys.stderr.write("Image {0} Posted to Slack\n".format(image_url))
-    # else:
-    #     if "headers" in resp:
-    #         hs = resp["headers"]
-    #         if "Retry-After" in hs:
-    #             delay = int(resp["headers"]["Retry-After"])
-    #             time.sleep(delay)
-    #             post_image_to_slack("MUY GUAPO!", image_url)
-    #         else:
-    #             raise Exception(ujson.dumps(resp))
-
-    return resp
-
-
-if __name__ == "__main__":
-    fdk.handle(handler)
+        res = await post_image_to_slack(msg, data.get("image_url"))
+    
+    return response.Response(
+        ctx, status_code=200, response_data=json.dumps(
+            {"ok": True}
+        ),
+        headers={"Content-Type": "application/json"}
+    )
+    #return res
